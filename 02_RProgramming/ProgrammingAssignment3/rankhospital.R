609330afc5ec23cf37bc36ba8177ec8209ca2015
Gregory Smetana <gsmetana@caltech.edu>
2014-10-26 23:00:02 -0700

    add rankhospital.R

---
+++ 02_RProgramming/ProgrammingAssignment3/rankhospital.R
@@ -0,0 +1,32 @@
+rankhospital <- function(state, outcome, num = "best") {
+        ## Read outcome data
+        ## Check that state and outcome are valid
+        ## Return hospital name in that state with the given rank
+        ## 30-day death rate
+        
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
+                stop("invalid outcome")
+        }
+        
+        if(!state %in% data$State){
+                stop("invalid state")
+        }
+        outcome <- switch(outcome,
+                          "pneumonia" ="Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia",
+                          "heart failure" ="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
+                          "heart attack" ="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" 
+        )
+        data <-  data[state == data$State, c("Hospital.Name", outcome)]
+        data[, outcome] <- as.numeric(data[, outcome])   
+        data <- data[order(data[,outcome], data$Hospital.Name), ]
+        data <- data[!is.na(data[outcome]),]
+                
+        if(num == "best"){
+                num <- 1
+        } else if(num == "worst"){
+                num <- length(data$Hospital.Name)
+        }
+        data[num, "Hospital.Name"]
+}
