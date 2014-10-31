3d68c0bc615340e4b10cfa2b55f6028c6f00c2f8
Gregory Smetana <gsmetana@caltech.edu>
2014-10-31 16:34:59 -0700

    redo using plyr

--- 02_RProgramming/ProgrammingAssignment3/rankall.R
+++ 02_RProgramming/ProgrammingAssignment3/rankall.R
@@ -5,6 +5,8 @@ rankall <- function(outcome, num = "best") {
         ## Return a data frame with the hospital names and the
         ## (abbreviated) state name
 
+        library(plyr)
+        
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
         if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) {
@@ -16,30 +18,31 @@ rankall <- function(outcome, num = "best") {
                           "heart failure" ="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
                           "heart attack" ="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" 
         )
+        if(num == "best"){
+                num <- 1
+        }
         
-        state <- unique(data$State)
-        state <- state[order(state)]
-                
-        data <-  data[, c("Hospital.Name", "State", outcome)]
-        data[, outcome] <- as.numeric(data[, outcome])   
-                
-        hospital <- character(0)
-        for(i in state){
-                tmp <-  data[i == data$State, c("Hospital.Name", outcome)]
-                tmp <- tmp[order(tmp[,outcome], tmp$Hospital.Name), ]
-                tmp <- tmp[!is.na(tmp[outcome]),]
-                
+        # remove tricky NAs
+        data[,outcome] <- as.numeric(data[, outcome])
+        data <-  data[!is.na(data[,outcome]), c("Hospital.Name","State",outcome)]
+        
+        # return the proper hospital for each state
+        orderHospitals <- function(df){ 
                 if(num == "best"){
                         n <- 1
                 } else if(num == "worst"){
-                        n <- length(tmp$Hospital.Name)
+                        n <- length(df$Hospital.Name)
                 } else{
                         n = num
                 }
-                hospital <-append(hospital, tmp[n, "Hospital.Name"] )
+                df[order(df[,outcome], df[,"Hospital.Name"])[n], "Hospital.Name"]
         }
         
-        result <- data.frame( hospital = hospital, state = state)
-        rownames(result) <- state
+        result <- ddply(data, c("State"), orderHospitals )
+        
+        # format result
+        result <- result[, c(2,1) ]
+        names(result) <- c("hospital", "state")
+        rownames(result) <- result$state
         result
 }
